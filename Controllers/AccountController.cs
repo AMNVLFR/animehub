eb9e00424b6410ae2369c3155fe5ec6336570014
AMNVLFR <mrniceguy1234337@gmail.com>
2025-10-05 01:00:56 +0900

    Fix build error by moving ResetPasswordViewModel to separate file

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -270,23 +270,4 @@ namespace AnimeHub.Controllers
             return View();
         }
     }
-
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; } = "";
-
-        [Required]
-        public string Token { get; set; } = "";
-
-        [Required]
-        [StringLength(100, MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        public string Password { get; set; } = "";
-
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; } = "";
-    }
 }
